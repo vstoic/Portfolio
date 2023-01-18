c9b18458e1881a0afe2dfb2058ecafc71a7d9a35
Sing Cheng <Sing.cheng@outlook.com>
2023-01-17 19:15:02 -0500

    main page

---
+++ src/components/AnimatedLetters/AnimatedLetters.jsx
@@ -0,0 +1,18 @@
+import './animatedLetters.scss';
+
+const AnimatedLetters = ({ letterClass, strArray, idx }) => {
+
+  return (
+    <span>
+      {
+        strArray.map((letter, i) => (
+          <span key={letter + i} className={`${letterClass} _${i + idx}`}>
+            {letter}
+          </span>
+        ))
+      }
+    </span>
+  )
+}
+
+export default AnimatedLetters;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,50 @@
 // import 'home.scss';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './home.scss';
+import LogoHome from '../Logo/LogoHome';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = [' ', 'V', 'i', 'c', 't', 'o', 'r'];
+  const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r', '.'];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+
+  }, []);
 
   return (
-    <div className='container home-page'>
-      <div className='text-zone'>
-        <h1>Hi, <br/> I'm Victor, <br/> Software Engineer.
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m </span>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={23}
+          />
         </h1>
         <h2> Frontend Engineer / JavaScript Expert / FullStack Engineer </h2>
-        <Link to='/contact' className='flat-button'>Contact Me</Link>
+        <Link to="/contact" className="flat-button">
+          Contact Me
+        </Link>
       </div>
+      <LogoHome/>
     </div>
-  )
+  );
 
 };
 
